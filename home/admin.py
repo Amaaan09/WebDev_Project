c4e079f058f48b8517655f2aec9e4085257f4a0e
ananya <ananya@letsaspire.in>
2022-10-20 20:10:33 +0530

    user

--- home/admin.py
+++ home/admin.py
@@ -3,4 +3,6 @@ from . import models
 
 # Register your models here.
 
-admin.site.register(models.User)
+admin.site.register(models.Profile)
+admin.site.register(models.Student)
+
--- home/models.py
+++ home/models.py
@@ -1,32 +1,34 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
-class User(models.Model):
+class Profile(models.Model):
     designation_choices = (
     ('student','STUDENT'),
     ('teacher', 'TEACHER')
     )
-    fullname = models.CharField(max_length=100)
+    user_name = models.ForeignKey(User,on_delete=models.CASCADE)
+    # fullname = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
     password = models.CharField(max_length=100)
     designation = models.CharField(max_length=7, choices=designation_choices, default='student')
 
     def __str__(self):
-        return self.fullname
+        return self.user_name
 
     class Meta:
-        verbose_name_plural = 'Users'
+        verbose_name_plural = 'Profile'
 
 
 class Student(models.Model):
-    fullname = models.CharField(max_length=100)
+    user_name = models.ForeignKey(User,on_delete=models.CASCADE)
+    # fullname = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
     password = models.CharField(max_length=100)
     hours_watched = models.FloatField(max_length=100)
 
     def __str__(self):
-        return self.fullname
+        return self.user_name
 
     class Meta:
         verbose_name_plural = 'Students'
