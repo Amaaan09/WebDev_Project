fe22ff36039d7bffa3a25d2230e9df67e3a87d3b
Amaan Ansari <95982010+Amaaan09@users.noreply.github.com>
2022-10-23 20:53:22 +0530

    Events Crud Completed
    BHAI BOHOT ERROR MILE BUT HO GAYA FINALLY

--- events/models.py
+++ events/models.py
@@ -4,14 +4,10 @@ from django.db import models
 
 class Projects(models.Model):
     name = models.CharField(max_length=100)
-    techStack = models.CharField(max_length=100, null=True, blank=True)
     desc = models.CharField(max_length=400)
-    link = models.CharField(max_length=50)
-    updated_at = models.DateTimeField(auto_now=True)
-    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name + ' ' + self.link
+        return self.name
  
     class Meta:
-        verbose_name_plural = 'Projects'
+        verbose_name_plural = 'Events'
