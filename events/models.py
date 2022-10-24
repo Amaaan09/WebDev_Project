ff43520aa08efb12d5dfaee8a9f8b6f46395caac
Amaan Ansari <95982010+Amaaan09@users.noreply.github.com>
2022-10-24 15:01:19 +0530

    Minor Changes
    Added time for Events,
    Made the forms better looking

--- events/models.py
+++ events/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Projects(models.Model):
     name = models.CharField(max_length=100)
     desc = models.CharField(max_length=400)
+    time = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return self.name
